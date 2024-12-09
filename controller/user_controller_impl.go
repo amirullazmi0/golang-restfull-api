198abc3a23157c81f6992109c564f4afeff78bb3
Amirull Azmi <84987345+amirullazmi0@users.noreply.github.com>
2024-12-09 12:41:46 +0700

    update controller

--- controller/user_controller_impl.go
+++ controller/user_controller_impl.go
@@ -1,6 +1,8 @@
 package controller
 
 import (
+	"amirullazmi/belajar-restfull-api/helper"
+	"amirullazmi/belajar-restfull-api/model/web"
 	"amirullazmi/belajar-restfull-api/service"
 	"net/http"
 
@@ -12,7 +14,17 @@ type UserControllerImpl struct {
 }
 
 func (controller *UserControllerImpl) Create(writer http.ResponseWriter, request *http.Request, params httprouter.Params) {
+	userCreateRequest := web.UserCreateRequest{}
+	helper.ReadFromRequestBody(request, &userCreateRequest)
 
+	user := controller.UserService.Create(request.Context(), userCreateRequest)
+	webResponse := web.WebResponse{
+		Code:   200,
+		Status: "OK",
+		Data:   user,
+	}
+
+	helper.WriteToResponseBody(writer, webResponse)
 }
 func (controller *UserControllerImpl) Update(writer http.ResponseWriter, request *http.Request, params httprouter.Params) {
 
